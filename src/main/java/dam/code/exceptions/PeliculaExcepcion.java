b739f2a37015ae6d8f19eff163974a0db6992a06
Marcomartinalvarez458 <marcomartinalvarez14@gmail.com>
2026-03-26 08:55:53 +0100

    arreglado

--- src/main/java/dam/code/exceptions/PeliculaExcepcion.java
+++ src/main/java/dam/code/exceptions/PeliculaExcepcion.java
@@ -1,5 +1,7 @@
 package dam.code.exceptions;
 
+//Excepcion personalizada para peliculas
+
 public class PeliculaExcepcion extends Exception {
     public PeliculaExcepcion(String message) {
         super(message);
--- src/main/java/dam/code/exceptions/PersonaExcepcion.java
+++ src/main/java/dam/code/exceptions/PersonaExcepcion.java
@@ -1,5 +1,6 @@
 package dam.code.exceptions;
 
+//Excepcion personalizada para personas
 public class PersonaExcepcion extends Exception {
     public PersonaExcepcion(String message) {
         super(message);
